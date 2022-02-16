f6783f1d9bf462941d92707a87baf9dc8c1db68b
Yongqi10 <q865984106@yahoo.com>
2022-02-16 17:51:33 -0500

    finish fetch card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Card() {
 
-    
+    const [card,setCard] = useState("");
+    const [loading, setLoading] = useState(true)
+    const getCard = async () =>{
+
+        try {
+        
+            const response = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Blue-Eyes")
+            const body = await response.json();
+            setCard(body.data);
+            setLoading(false)
+
+        } catch (err) {
+            console.error(err.message)
+        }
+
+    }
+
+    useEffect(()=>{
+
+        getCard()
+        
+    },[])
+    console.log(card)
 
     return (
-        <div>Card</div>
+        <div>
+        {
+            loading === false?
+            card.map((item,index)=>{
+
+                return (<img src={item.card_images[0].image_url_small} key = {item+index} alt = ""/>)
+
+
+            }):<p>Loading</p>
+        }
+
+        </div>
     );
 }
 
